2528fdf0485c46e5a7964d4b99d203b597c75036
SamLi-hub <hocom.inn@outlook.com>
2024-12-13 09:46:32 +0800

    JavaQuest3.java

---
+++ exercise/JavaQuest2.java
@@ -0,0 +1,38 @@
+/**
+ * Expected Output:
+ * 
+ * a > 9
+ * str length < 27
+ */
+// Primitive types and operators
+public class JavaQuest2 {
+  public static void main(String[] args) {
+    int a = 10;
+    if (a == 9) {
+      System.out.println("a is equals to 9");
+    } else if (a >= 0 && a <= 8) {
+      System.out.println("a is between 0 and 8");
+    // how about a > 9?
+    } else if (a > 9) {
+      System.out.println("a > 9");
+    }
+    else if (a < 0){
+      System.out.println("a < 0");
+    }
+
+    String str = "I love Java. I love coding";
+    if () {
+      System.out.println("str length=27");
+    } else if () { // code here ... add the condition
+      System.out.println("str length < 27");
+    } else if () { // code here ... complete the code
+      System.out.println("str length > 27");
+    }
+  }
+
+
+
+
+}
+  
+}
